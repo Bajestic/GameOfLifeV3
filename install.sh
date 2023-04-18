ae5bacf7174936ed859f55b1d5a32dc10891e594
bajestic <lukasz.przybyla@hotmail.com>
2023-04-18 17:08:05 +0200

    Add GTest module and CMake target for testing

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-BUILD_DIR=./build
+PROJECT_DIR=$(pwd)
+SRC_DIR=${PROJECT_DIR}/GameOfLife
+BUILD_DIR=${PROJECT_DIR}/build
 GCC_COMPILER_PATH=$(which gcc)
 cmake -D CMAKE_C_COMPILER="${GCC_COMPILER_PATH}" -D CMAKE_CXX_COMPILER="${GCC_COMPILER_PATH}" ./GameOfLife/CMakeLists.txt
 
@@ -16,9 +18,23 @@ else
     cd ${BUILD_DIR}
 fi
 
-cmake ../GameOfLife -B./
-pwd
+if [ "$#" -eq 0 ]; then cmake -B . -S${SRC_DIR}
+elif [ $1 == "GameOfLife" ]; then cmake -B${BUILD_DIR} -S${SRC_DIR} #--target GameOfLife 
+elif [ $1 == "GameOfLife_UT" ]; then cmake -B${BUILD_DIR} -S${SRC_DIR} #--target GameOfLife_UT
+elif [ $1 ]; then echo "Bad target!"; exit 3
+fi
+
 make
-echo "Installed successful"
-cd ../GameOfLife/project
-rm -fdr CMakeFiles cmake_install.cmake CMakeCache.txt compile_commands.json Makefile
+TO_CLEANUP="CMakeFiles cmake_install.cmake CMakeCache.txt compile_commands.json Makefile CTestTestfile.cmake"
+
+cd ${SRC_DIR}
+rm -fdr ${TO_CLEANUP}
+
+cd ${SRC_DIR}/project
+rm -fdr ${TO_CLEANUP}
+
+cd ${SRC_DIR}/tests
+rm -fdr ${TO_CLEANUP}
+
+cd ${BUILD_DIR}/project; ./GameOfLife
+cd ${BUILD_DIR}/tests; ./GameOfLife_UT
